2d1d6a621450a484522788e313e32f2bdeefae19
Kanha <dhvanit03@gmail.com>
2023-09-02 08:13:50 +0530

    make it more sufficient

--- bubbleSorting.c
+++ bubbleSorting.c
@@ -9,20 +9,28 @@ void printArr(int *A,int n)
 }
 void bubbleSort(int *A,int n)
 {
+    int check;
     for(int i=0;i<n;i++)
-    {
+    {   
+        check=1;
+        printf("this is %d itration\n",i+1);
         for(int j=0;j<n-1-i;j++)
         {
             if(A[j]>A[j+1])
             {
                 A[j]=(A[j]+A[j+1])-(A[j+1]=A[j]);
+                check=0;
             }
+        }
+        if(check)
+        {
+            return;
         }
     }
 }
 int main()
 {
-    int arr[]={12,44,6,23,676,22};
+    int arr[]={12,44,6,23,676,999};
     int n=6;
     printf("Before sorting : ");
     printArr(arr,n);
